e67ac24f21073b710d59cb42c66c33b3b0b918ee
jhoelber <jho.elber@hotmail.com>
2025-10-03 20:25:32 -0300

    arrumando carrossel

--- src/components/Carrossel2.tsx
+++ src/components/Carrossel2.tsx
@@ -12,7 +12,7 @@ import {
 
 export function Carousel2() {
   const plugin = React.useRef(
-    Autoplay({ delay: 3000, stopOnInteraction: true })
+    Autoplay({ delay: 1700, stopOnInteraction: true })
   )
 
   return (
@@ -23,70 +23,28 @@ export function Carousel2() {
     >
       {/* gap entre os itens */}
       <CarouselContent className="-ml-2">
-
-        <CarouselItem className="pl-2 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/3 flex justify-center">
-          <img
-            src="/carrosselSection1/TerceiraColorir3d.jpg"
-            alt="Crianças pintando"
-            className="max-w-[260px] h-auto object-contain rounded-xl"
-          />
-        </CarouselItem>
-
-        <CarouselItem className="pl-2 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/3 flex justify-center">
-          <img
-            src="/carrosselSection1/Crianca1.jpg"
-            alt="Crianças pintando"
-            className="max-w-[260px] h-auto object-contain rounded-xl"
-          />
-        </CarouselItem>
-
-        <CarouselItem className="pl-2 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/3 flex justify-center">
-          <img
-            src="/carrosselSection1/Grito.jpg"
-            alt="Logo do site"
-            className="max-w-[260px] h-auto object-contain rounded-xl"
-          />
-        </CarouselItem>
-
-
-        <CarouselItem className="pl-2 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/3 flex justify-center">
-          <img
-            src="/carrosselSection1/Crianca2.jpg"
-            alt="Crianças pintando"
-            className="max-w-[260px] h-auto object-contain rounded-xl"
-          />
-        </CarouselItem>
-
-        <CarouselItem className="pl-2 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/3 flex justify-center">
-          <img
-            src="/carrosselSection1/Mulher.jpg"
-            alt="Crianças pintando"
-            className="max-w-[260px] h-auto object-contain rounded-xl"
-          />
-        </CarouselItem>
-        <CarouselItem className="pl-2 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/3 flex justify-center">
-          <img
-            src="/carrosselSection1/DesenhoPintado.jpg"
-            alt="Crianças pintando"
-            className="max-w-[260px] h-auto object-contain rounded-xl"
-          />
-        </CarouselItem>
-        <CarouselItem className="pl-2 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/3 flex justify-center">
-          <img
-            src="/carrosselSection1/QuintaColorir3d.jpg"
-            alt="Crianças pintando"
-            className="max-w-[260px] h-auto object-contain rounded-xl"
-          />
-        </CarouselItem>
-
-
-        <CarouselItem className="pl-2 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/3 flex justify-center">
-          <img
-            src="/carrosselSection1/Bloqueado.png"
-            alt="Crianças pintando"
-            className="max-w-[260px] h-auto object-contain rounded-xl m-6"
-          />
-        </CarouselItem>
+        {[
+          "/carrosselSection1/TerceiraColorir3d.jpg",
+          "/carrosselSection1/Crianca1.jpg",
+          "/carrosselSection1/Grito.jpg",
+          "/carrosselSection1/Crianca2.jpg",
+          "/carrosselSection1/Mulher.jpg",
+          "/carrosselSection1/DesenhoPintado.jpg",
+          "/carrosselSection1/QuintaColorir3d.jpg",
+          "/carrosselSection1/Bloqueado.png",
+        ].map((src, i) => (
+          <CarouselItem
+            key={i}
+            className="pl-2 shrink-0 basis-1/2 sm:basis-1/3 md:basis-1/3 lg:basis-1/3 flex justify-center"
+          >
+            <img
+              src={src}
+              alt="slide"
+              className="w-full max-w-[260px] h-auto object-contain rounded-xl"
+              draggable={false}
+            />
+          </CarouselItem>
+        ))}
       </CarouselContent>
 
       <CarouselPrevious />
